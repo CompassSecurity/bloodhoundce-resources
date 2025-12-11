31664dc66babd014b9cd6e4de0ccf6d2ed43fc2f
Emanuel Duss <me@emanuelduss.ch>
2025-12-11 09:43:58 +0100

    Create BloodHound Operator Session using username/password instead of API key

--- scripts/Create-BloodHoundOperatorSession.ps1
+++ scripts/Create-BloodHoundOperatorSession.ps1
@@ -4,16 +4,37 @@
 # Connect to BloodHound API using BloodHoundOperator.
 #
 
-Write-Host "[*] Loading BloodHoundOperator..." -ForeGroundColor Green
+param(
+    [string]$Username = "admin",
+
+    [Parameter(Mandatory = $true)]
+    [string]$Password,
+
+    [string]$Hostname = "127.0.0.1",
+
+    [int]$Port = 8080
+)
+
+Write-Host "[*] Authenticate..." -ForeGroundColor Green
+$body = @{
+    login_method = "secret"
+    username     = $Username
+    secret       = $Password
+} | ConvertTo-Json
+
+$response = Invoke-WebRequest `
+    -Uri "http://$Hostname`:$Port/api/v2/login" `
+    -Method POST `
+    -Body $body `
+    -ContentType "application/json"
+
+$token = ($response.Content | ConvertFrom-Json).data.session_token
+
+Write-Host "[*] Loading BloodHoundOperator module..." -ForeGroundColor Green
 Import-Module /opt/BloodHoundOperator/BloodHoundOperator.ps1
 
-$BHTokenKey = "WW91ciBCbG9vZEhvdW5kIEFQSSBLZXkgY29tZXMgaGVyZSA6KQ=="
-$BHTokenID = "596F7572-2054-6F6B-656E-204944203A29"
-$BHServer = "127.0.0.1"
-$BHPort = "8080"
-
 Write-Host "[*] Logging in..." -ForeGroundColor Green
-New-BHSession -Server $BHServer -Port $BHPort -TokenID $BHTokenID -Token (ConvertTo-SecureString -AsPlainText -Force $BHTokenKey)
+New-BHSession -Server $Hostname -Port $Port -JWT $token
 
 Write-Host "[*] Your session:" -ForeGroundColor Green
 Get-BHSession
